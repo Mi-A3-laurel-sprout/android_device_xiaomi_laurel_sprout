1ff1762e8ebf7172cb633ff2429a314ea6f31331
Magicxavi <rolfpeterkesche@gmail.com>
2023-05-25 06:41:25 +0300

    laurel_sprout: drop performance blobs

--- device.mk
+++ device.mk
@@ -393,8 +393,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.laurel_sprout-libperfmgr \
-    android.hardware.power.stats@1.0-service.pixel \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power.stats@1.0-service.pixel
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
