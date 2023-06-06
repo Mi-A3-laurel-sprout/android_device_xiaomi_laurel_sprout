306bfe10821b1e3eb16b2830889336e030a9b3d4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-06-06 23:21:47 +0300

    laurel_sprout: Implement udfps handler

--- device.mk
+++ device.mk
@@ -269,7 +269,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint feature
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
+    libudfpshandler
 
 # FM
 PRODUCT_PACKAGES += \
