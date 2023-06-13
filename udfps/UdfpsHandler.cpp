e02914ccb11dc2c1f9fff289fa668b4cf9cf0ad0
Magicxavi <rolfpeterkesche@gmail.com>
2023-06-13 04:34:44 +0300

    laurel_sprout: udfps: define FOD_STATUS_PATH node from kernel
    Also cleanup fod_ui and fod_status handling script.

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -31,89 +31,20 @@
 #define FOD_DIM_ON 1
 #define FOD_DIM_OFF 0
 
+#define FOD_STATUS_PATH "/sys/class/touch/tp_dev/fod_status"
+#define FOD_STATUS_ON 1
+#define FOD_STATUS_OFF 0
+
 template <typename T>
 static void set(const std::string& path, const T& value) {
     std::ofstream file(path);
     file << value;
 }
 
-static const char* kFodUiPaths[] = {
-        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui",
-        "/sys/devices/platform/soc/soc:qcom,dsi-display/fod_ui",
-};
-
-static const char* kFodStatusPaths[] = {
-        "/sys/devices/virtual/touch/tp_dev/fod_status",
-        "/sys/class/touch/tp_dev/fod_status",
-};
-
-static bool readBool(int fd) {
-    char c;
-    int rc;
-
-    rc = lseek(fd, 0, SEEK_SET);
-    if (rc) {
-        LOG(ERROR) << "failed to seek fd, err: " << rc;
-        return false;
-    }
-
-    rc = read(fd, &c, sizeof(char));
-    if (rc != 1) {
-        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
-        return false;
-    }
-
-    return c != '0';
-}
-
 class LaurelSproutUdfpsHander : public UdfpsHandler {
   public:
     void init(fingerprint_device_t *device) {
         mDevice = device;
-
-        std::thread([this]() {
-            int fodUiFd;
-            for (auto& path : kFodUiPaths) {
-                fodUiFd = open(path, O_RDONLY);
-                if (fodUiFd >= 0) {
-                    break;
-                }
-            }
-
-            if (fodUiFd < 0) {
-                LOG(ERROR) << "failed to open fd, err: " << fodUiFd;
-                return;
-            }
-
-            int fodStatusFd;
-            for (auto& path : kFodStatusPaths) {
-                fodStatusFd = open(path, O_RDWR);
-                if (fodStatusFd >= 0) {
-                    break;
-                }
-            }
-
-            struct pollfd fodUiPoll = {
-                    .fd = fodUiFd,
-                    .events = POLLERR | POLLPRI,
-                    .revents = 0,
-            };
-
-            while (true) {
-                int rc = poll(&fodUiPoll, 1, -1);
-                if (rc < 0) {
-                    LOG(ERROR) << "failed to poll fd, err: " << rc;
-                    continue;
-                }
-
-                bool fodUi = readBool(fodUiFd);
-
-                mDevice->extCmd(mDevice, COMMAND_NIT, fodUi ? PARAM_NIT_FOD : PARAM_NIT_NONE);
-                if (fodStatusFd >= 0) {
-                    write(fodStatusFd, fodUi ? "1" : "0", 1);
-                }
-            }
-        }).detach();
     }
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
