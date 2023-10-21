e26fbed0623a449271cbeedfc9afe589ab5e7f66
Magicxavi <rolfpeterkesche@gmail.com>
2023-10-21 21:11:49 +0300

    laurel_sprout: move to stock camera stack for now

--- device.mk
+++ device.mk
@@ -196,13 +196,14 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     android.hidl.memory.block@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++_vendor \
     libcamera2ndk_vendor \
     libdng_sdk.vendor
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.camera.common@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.camera.common@1.0.so \
+    prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-core/android.hardware.camera.common@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.common@1.0.so
 
 # Consumerir
 PRODUCT_PACKAGES += \
