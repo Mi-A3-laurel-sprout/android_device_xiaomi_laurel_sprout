1a2a5ebd1aafa497c20d4e78752cfd809027d970
magicxavi <rolfpeterkesche@gmail.com>
2024-06-20 18:44:00 +0300

    laurel_sprout: set stock build fingerprint

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -41,7 +41,7 @@ TARGET_VENDOR_PRODUCT_NAME := laurel_sprout
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="laurel_sprout-user 11 RKQ1.200903.002 V12.0.15.0.RFQMIXM release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/laurel_sprout/laurel_sprout:11/RKQ1.200903.002/V12.0.15.0.RFQMIXM:user/release-keys
 
 PRODUCT_PACKAGES += \
     RemovePackages \
