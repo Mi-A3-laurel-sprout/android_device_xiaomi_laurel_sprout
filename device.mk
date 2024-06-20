27b464dc137462a944367660ee9a52b589768585
Magicxavi <rolfpeterkesche@gmail.com>
2024-06-20 22:24:19 +0300

    laurel_sprout: build xiaomi vibrator service

--- device.mk
+++ device.mk
@@ -525,7 +525,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)//etc/excluded-input-devices.xml
